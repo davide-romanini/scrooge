b6fb7aba1417ee60859df62e169ed9e22a2d3771
Davide Romanini <d.romanini@cineca.it>
2014-12-22 16:18:45 +0100

    json ld series

--- src/Coa/ScroogeBundle/Controller/ApiController.php
+++ src/Coa/ScroogeBundle/Controller/ApiController.php
@@ -2,7 +2,7 @@
 
 namespace Coa\ScroogeBundle\Controller;
 
-use Doctrine\Common\Util\Debug;
+use Coa\ScroogeBundle\Entity\Publication;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,10 +16,30 @@ class ApiController extends Controller
     public function seriesDetailsAction($countrycode, $localcode)
     {
         $publicationcode = "$countrycode/$localcode";
+        /* @var $pub Publication */
         $pub = $this->getDoctrine()->getManager()->getRepository("CoaScroogeBundle:Publication")
                 ->find($publicationcode);
         if(!$pub) throw $this->createNotFoundException("The series [$publicationcode] does not exist");
-        Debug::dump($pub);
-        return new Response();
+        
+        $s = $this->toJson([
+            '@type' => ['ComicSeries', 'Periodical'],
+            'name' => $pub->getTitle(),
+            'comment' => $pub->getPublicationcomment(),
+            'inLanguage' => $pub->getLanguagecode()
+        ]);
+        $r = new Response(json_encode($s));
+        $r->headers->set('Content-type', 'application/ld+json');
+        return $r;
+    }
+    
+    /**
+     * Merges data with schema.org context and json ld boilerplate
+     * 
+     * @param array $a
+     */
+    protected function toJson(array $a) 
+    {
+        $a["@context"] = "http://schema.org/";
+        return $a;
     }
 }
