ddf937410211721a41c7d515c0d29d529fecedbf
Davide Romanini <d.romanini@cineca.it>
2014-12-22 16:45:22 +0100

    issue prototype (incomplete)

--- src/Coa/ScroogeBundle/Controller/ApiController.php
+++ src/Coa/ScroogeBundle/Controller/ApiController.php
@@ -2,18 +2,20 @@
 
 namespace Coa\ScroogeBundle\Controller;
 
+use Coa\ScroogeBundle\Entity\Issue;
 use Coa\ScroogeBundle\Entity\Publication;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ApiController extends Controller
 {
     
     /**
-     * @Route("/series/{countrycode}/{localcode}") 
+     * @Route("/series/{countrycode}/{localcode}", name="series_detail") 
      */
-    public function seriesDetailsAction($countrycode, $localcode)
+    public function seriesDetailAction($countrycode, $localcode)
     {
         $publicationcode = "$countrycode/$localcode";
         /* @var $pub Publication */
@@ -28,6 +30,37 @@ class ApiController extends Controller
             'inLanguage' => $pub->getLanguagecode()
         ]);
         $r = new Response(json_encode($s));
+        $r->headers->set('Content-type', 'application/ld+json');
+        return $r;
+    }
+    
+    /**
+     * @Route("/series/{countrycode}/{localcode}/issues/{issuenumber}") 
+     */
+    public function issueDetailsAction($countrycode, $localcode, $issuenumber)
+    {
+        $publicationcode = "$countrycode/$localcode";
+        
+        /* @var $is Issue */
+        $is = $this->getDoctrine()->getManager()->getRepository("CoaScroogeBundle:Issue")
+                ->findBy([
+                    'publication' => $publicationcode,
+                    'issuenumber' => $issuenumber
+        ]);
+        if(empty($is)) throw $this->createNotFoundException ("The issue [$publicationcode] number #$issuenumber does not exist");
+        $is = $is[0];
+        
+        $i = $this->toJson([
+            '@type' => ['ComicIssue', 'PublicationIssue'],
+            'name' => $is->getTitle(),
+            'isPartOf' => $this->generateUrl('series_detail', [
+                'countrycode' => $countrycode, 
+                'localcode' => $localcode
+            ], UrlGeneratorInterface::ABSOLUTE_URL),
+            'comment' => $is->getIssuecomment(),
+            'pageEnd' => $is->getPages(),
+        ]);
+        $r = new Response(json_encode($i));
         $r->headers->set('Content-type', 'application/ld+json');
         return $r;
     }
---
+++ src/Coa/ScroogeBundle/Entity/Issue.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Coa\ScroogeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Issue
+ *
+ * @ORM\Entity(readOnly=true)
+ * @ORM\Table(name="INDUCKS_ISSUE")
+ * 
+ * @author dromanin
+ */
+class Issue 
+{
+    /**
+     * @ORM\Column(type="string")
+     * @ORM\Id
+     */
+    private $issuecode;
+    
+    /**
+     * @ORM\ManyToOne(targetEntity="Publication")
+     * @ORM\JoinColumn(name="publicationcode", referencedColumnName="publicationcode")
+     */
+    private $publication;
+    /** @ORM\Column **/
+    private $issuenumber;
+    /** @ORM\Column **/
+    private $title;
+    /** @ORM\Column **/
+    private $size;
+    /** @ORM\Column **/
+    private $pages;
+    /** @ORM\Column **/
+    private $price;
+    /** @ORM\Column **/
+    private $printrun;
+    /** @ORM\Column **/
+    private $attached;
+    /** @ORM\Column **/
+    private $oldestdate;
+    /** @ORM\Column **/
+    private $issuecomment;
+    
+    function getIssuecode() {
+        return $this->issuecode;
+    }
+
+    function getPublication() {
+        return $this->publication;
+    }
+
+    function getIssuenumber() {
+        return $this->issuenumber;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getSize() {
+        return $this->size;
+    }
+
+    function getPages() {
+        return $this->pages;
+    }
+
+    function getPrice() {
+        return $this->price;
+    }
+
+    function getPrintrun() {
+        return $this->printrun;
+    }
+
+    function getAttached() {
+        return $this->attached;
+    }
+
+    function getOldestdate() {
+        return $this->oldestdate;
+    }
+
+    function getIssuecomment() {
+        return $this->issuecomment;
+    }
+
+
+
+}
