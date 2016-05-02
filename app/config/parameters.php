0fb02fac70153a1144470207e4065198a1238e85
Davide Romanini <d.romanini@cineca.it>
2016-05-02 12:00:22 +0200

    abandoning sqlite, automatic mysql update

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,4 +1,7 @@
 <?php
 if (getenv('OPENSHIFT_APP_NAME')) {
-  $container->setParameter('database_path', getenv("OPENSHIFT_DATA_DIR") . "/coa.sqlite");
+  $container->setParameter('database_host', getenv("OPENSHIFT_MYSQL_DB_HOST"));
+  $container->setParameter('database_port', getenv("OPENSHIFT_MYSQL_DB_PORT"));
+  $container->setParameter('database_user', getenv("OPENSHIFT_MYSQL_DB_USERNAME"));
+  $container->setParameter('database_password', getenv("OPENSHIFT_MYSQL_DB_PASSWORD"));
 }
