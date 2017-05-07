e5d7905e42411eefe684520eb66576fd68264ff0
Davide Romanini <davide.romanini@gmail.com>
2017-05-07 11:19:13 +0200

    add initial docker configuration. remove useless stuff. upgrade to php7.1 and symfony 2.8.

--- .openshift/cron/daily/updatedb.sh
+++ .openshift/cron/daily/updatedb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /tmp
-curl http://coa.inducks.org/inducks/isv.tgz -o isv.tgz
-curl http://coa.inducks.org/inducks/isv/createtables.sql -o createtables.sql
+curl https://coa.inducks.org/inducks/isv.tgz -o isv.tgz
+curl https://coa.inducks.org/inducks/isv/createtables.sql -o createtables.sql
 tar xfz isv.tgz
 echo "SET names 'utf8'; SOURCE createtables.sql;"|mysql -f -u $OPENSHIFT_MYSQL_DB_USERNAME -h $OPENSHIFT_MYSQL_DB_HOST --password=$OPENSHIFT_MYSQL_DB_PASSWORD --default-character-set=utf8
 rm createtables.sql
