39f888efe1b9566295f1f638a0b89a1f66bd19b4
Davide Romanini <davide.romanini@gmail.com>
2017-05-07 12:19:38 +0200

    upgrade to Symfony 3.2

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -7,17 +7,14 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
+        $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Coa\ScroogeBundle\CoaScroogeBundle(),
-        );
+        ];
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
@@ -28,7 +25,17 @@ class AppKernel extends Kernel
 
         return $bundles;
     }
+    
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
 
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+    
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
--- web/app_dev.php
+++ web/app_dev.php
@@ -8,10 +8,9 @@ use Symfony\Component\Debug\Debug;
 //umask(0000);
 
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+/** @var \Composer\Autoload\ClassLoader $loader */
+$loader = require __DIR__.'/../app/autoload.php';
 Debug::enable();
-
-require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
